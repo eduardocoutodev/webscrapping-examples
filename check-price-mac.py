d740160351044ef2f7c5ebc1058deeb45d66da48
Eduardo Couto <ecouto93@gmail.com>
2023-10-22 18:31:25 +0100

    chore: created script to check price of macs

--- check-price-mac.py
+++ check-price-mac.py
@@ -3,11 +3,21 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 import chromedriver_autoinstaller
+import requests
+from dotenv import load_dotenv
+import os
+import json
+from selenium.common.exceptions import NoSuchElementException
+import re
 
 macbook_urls = [
-    'https://www.apple.com/shop/product/G11C2LL/A/refurbished-133-inch-macbook-pro-apple-m1-chip-with-8‑core-cpu-and-8‑core-gpu-space-gray']
+    'https://www.apple.com/shop/product/G11C2LL/A/refurbished-133-inch-macbook-pro-apple-m1-chip-with-8‑core-cpu-and-8‑core-gpu-space-gray', 'https://www.apple.com/shop/product/G11B0LL/A/refurbished-133-inch-macbook-pro-apple-m1-chip-with-8‑core-cpu-and-8‑core-gpu-space-gray', 'https://www.apple.com/ie/shop/product/G11C3B/A/refurbished-133-inch-macbook-pro-apple-m1-chip-with-8‑core-cpu-and-8‑core-gpu-space-grey?fnode=7880e9223c8ca882d116353105cf2617f579ddb56b91917897e2b78d51eeb99e012e2b535eaa193793b481d5a20bb63b38b21d7fb05b083c84a52ffda8f8c0a2b589462f851e14eeda6c5e023cdce100', 'https://www.apple.com/ie/shop/product/FPHJ3B/A/refurbished-14-inch-macbook-pro-apple-m2-pro-chip-with-12-core-cpu-and-19-core-gpu-silver?fnode=7880e9223c8ca882d116353105cf2617f579ddb56b91917897e2b78d51eeb99e012e2b535eaa193793b481d5a20bb63b38b21d7fb05b083c84a52ffda8f8c0a2b589462f851e14eeda6c5e023cdce100']
+
+load_dotenv()
+
+NTFY_TOPIC_NAME = os.getenv('NTFY_TOPIC')
+NFTY_EMAIL_TO_SEND = os.getenv("NFTY_EMAIL_TO_SEND")
 
 
 def main():
@@ -33,24 +43,77 @@ def main():
 
     for url in macbook_urls:
         access_url(url, browser)
+        # Give 5 seconds interval in between
+        time.sleep(5)
+
+    browser.quit()
 
 
-def access_url(url, browser):
+def access_url(url: str, browser):
     try:
+        print(f"Started Url: {url}")
         browser.get(url)
 
         time.sleep(5)
 
         # Get the current price of macbook
-        current_price = browser.find_element(
-            By.CSS_SELECTOR, ".rc-prices-fullprice").text
+        current_price = extractPrices(browser.find_element(
+            By.CSS_SELECTOR, ".rc-prices-fullprice").text)
 
-        print(f"Current Price is {current_price}")
+        try:
+            previous_price = extractPrices(browser.find_element(
+                By.CSS_SELECTOR, ".rc-prices-currentprice .as-price-previousprice"
+            ).text)
+
+            savings = extractPrices(browser.find_element(
+                By.CSS_SELECTOR, ".rc-prices-currentprice .rc-prices-savings"
+            ).text)
+
+            discountPercentage = round(
+                float(savings) / float(previous_price), 2) * 100
+
+            message_to_send = f"""Macbook with 16 GB Ram, 512 SSD and 13 inches.
+            Current Price: {current_price}
+            Old Price: {previous_price}
+            Saving: {savings}
+            
+            Saving percentage: {discountPercentage}%
+            Link: {url}"""
+
+            body = json.dumps({
+                "topic": NTFY_TOPIC_NAME,
+                "message": message_to_send,
+                "title": "Macbook New Discount",
+                "actions": [
+                    {
+                        "action": "view",
+                        "label": "Open Macbook Discount",
+                        "url": url,
+                    }
+                ]
+            })
+
+            headers = {
+                # "Email": NFTY_EMAIL_TO_SEND
+            }
+
+            requests.post("https://ntfy.sh/", data=body, headers=headers)
+
+        except NoSuchElementException:
+            print("Element not found")
 
     except Exception as e:
         print(e)
-    finally:
-        browser.quit()
+
+
+def extractPrices(text: str) -> str:
+    match = re.search(r'[0-9,]+.[0-9]', text)
+
+    if match:
+        # Need to remove ',' from prices, need to parse 3,099.0 to 3099.0
+        return match.group().replace(",", "")
+
+    return ""
 
 
 if __name__ == "__main__":
